1507b340b0442abcc959bed797e181320b532503
Anders Fischer-Nielsen <andersfischern@me.com>
2022-06-23 11:52:13 +0200

    Only use TypeScript syntax check

--- dangerfile.ts
+++ dangerfile.ts
@@ -9,9 +9,9 @@ const modified = danger.git.modified_files;
 (async () => {
   const isDraft = (
     await github.api.pulls.get({
-      owner: pr.head?.repo?.owner.login,
-      repo: pr.head?.repo?.name,
-      pull_number: pr?.number,
+      owner: github.thisPR?.owner,
+      repo: github.thisPR?.repo,
+      pull_number: github.thisPR?.number,
     })
   )?.data?.draft;
 
