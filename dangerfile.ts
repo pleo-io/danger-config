d375496f6fed225d91f034b3da762de00ad6bed1
Anders Fischer-Nielsen <andersfischern@me.com>
2022-07-15 15:51:47 +0200

    Ensure emoji uniformity

--- dangerfile.ts
+++ dangerfile.ts
@@ -75,9 +75,9 @@ const modified = danger.git.modified_files;
   }
 
   if (willShowGuidelines) {
-    const message = `Good work! ❤️ If you are in wondering why these messages appear, [check out our PR guidelines](https://www.notion.so/pleo/PR-and-Code-Review-Culture-at-Pleo-220324344eb849f3b636cd00a28b4a41)! 📚 
+    const message = `❤️ Good work! 📚 If you are in wondering why these messages appear, [check out our PR guidelines](https://www.notion.so/pleo/PR-and-Code-Review-Culture-at-Pleo-220324344eb849f3b636cd00a28b4a41)! 
 
-📄 _PR checks are defined in [our Dangerfile](https://github.com/pleo-io/danger-config/blob/master/dangerfile.ts). Reach out to [#engprod-devexp](https://getpleo.slack.com/archives/C030H8BMU8K) for questions._
+📄 _PR checks are defined in [our Dangerfile](https://github.com/pleo-io/danger-config/blob/master/dangerfile.ts). 📢 Reach out to [#engprod-devexp](https://getpleo.slack.com/archives/C030H8BMU8K) for questions._
 
 `;
     markdown(message);
